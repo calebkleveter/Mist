9e83d07967ce89f8ed8d02a2ebda79c31390bad2
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-11 16:58:40 -0500

    Package.swift - Added PostgreSQL Provider and AdminPanel

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@ import PackageDescription
 let package = Package(
     name: "mist",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/calebkleveter/postgres-admin-panel.git", Version(0,8,8))
     ],
     exclude: [
         "Config",
@@ -11,6 +13,5 @@ let package = Package(
         "Localization",
         "Public",
         "Resources",
-    ]
+        ]
 )
-
