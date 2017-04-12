8e5a6d7ecdb0b8e34461be300b5e887a46567ec9
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-12 16:00:11 -0500

    Package.swift - Updated AdminPanel package to version 0.9.0 (local)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/calebkleveter/postgres-admin-panel.git", Version(0,8,8))
+        .Package(url: "../../packages/postgres-admin-panel/.git", Version(0,9,0))
     ],
     exclude: [
         "Config",
