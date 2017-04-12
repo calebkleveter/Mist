46728befb2be6af28e2fde6ebedb3b771ec763d4
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-12 12:48:25 -0500

    Sources/App/main.swift - Added AdminPanel provider to droplet

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -5,6 +5,7 @@ import AdminPanel
 let drop = Droplet()
 
 try drop.addProvider(VaporPostgreSQL.Provider.self)
+try drop.addProvider(AdminPanel.Provider.self)
 
 drop.get { req in
     return try drop.view.make("welcome", [
