f54bf6581dd2b00264625bac34bc686fa77af20d
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-12 12:47:51 -0500

    Sources/App/main.swift - Added  VaporPostgreSQL provider to droplet

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -4,6 +4,8 @@ import AdminPanel
 
 let drop = Droplet()
 
+try drop.addProvider(VaporPostgreSQL.Provider.self)
+
 drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
