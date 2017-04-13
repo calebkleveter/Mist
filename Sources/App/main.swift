0ee6bfb14b88161954ff02399285a0bc3edb449a
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-13 11:39:59 -0500

    Added BlogPage to droplet preparations

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -7,6 +7,8 @@ let drop = Droplet()
 try drop.addProvider(VaporPostgreSQL.Provider.self)
 try drop.addProvider(AdminPanel.Provider.self)
 
+drop.preparations.append(BlogPage.self)
+
 drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
