2dcb72877877ba8d1cddc1bf27ac82c899a2f8ca
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-13 11:50:01 -0500

    Sources/App/Controllers/DashboardController+Mist.swift - Created MistDashboardController.createNewPage(request)

--- Sources/App/Controllers/DashboardController+Mist.swift
+++ Sources/App/Controllers/DashboardController+Mist.swift
@@ -6,5 +6,20 @@ import VaporPostgreSQL
 import Flash
 
 class MistDashboardController {
-
+    
+    func createNewPage(_ request: Request)throws -> ResponseRepresentable {
+        guard let pageName = request.data["page-name"]?.string,
+            let pageContent = request.data["page-content"]?.string else {
+                throw Abort.badRequest
+        }
+        
+        var newPage = BlogPage(title: pageName, content: pageContent)
+        do {
+            try newPage.save()
+        } catch let error {
+            return Response(redirect: "/admin/dashboard/cnew-page").flash(.error, "An error occured while saving the page: \(error)")
+        }
+        
+        return Response(redirect: "/admin/dashboard/cnew-page").flash(.success, "Page succesfully created!")
+    }
 }
