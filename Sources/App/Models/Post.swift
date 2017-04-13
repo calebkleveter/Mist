b64bb28cca5dbd6b6313afb1b9691fb7573a2cda
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-13 12:53:52 -0500

    Added contentCard String var to Post class

--- Sources/App/Models/Post.swift
+++ Sources/App/Models/Post.swift
@@ -6,23 +6,28 @@ final class Post: Model {
     var id: Node?
     var content: String
     var name: String
+    var contentCard: String!
     
     init(content: String, name: String) {
         self.content = content
         self.name = name
+        
+        if self.content.characters.count > 500 { self.contentCard = self.content.substring(to: content.index(content.startIndex, offsetBy: 497)) + "..." } else { self.contentCard = self.content }
     }
 
     init(node: Node, in context: Context) throws {
         id = try node.extract("id")
         content = try node.extract("content")
         name = try node.extract("name")
+        contentCard = try node.extract("contentCard")
     }
 
     func makeNode(context: Context) throws -> Node {
         return try Node(node: [
             "id": id,
             "content": content,
-            "name": name
+            "name": name,
+            "contentCard": contentCard
         ])
     }
 }
