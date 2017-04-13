51d251afa24210e1aaf758928faf0c3540f387e2
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-13 11:38:25 -0500

    Sources/App/Models/Page.swift - Implimented Model protocol on BlogPage

--- Sources/App/Models/Page.swift
+++ Sources/App/Models/Page.swift
@@ -1,12 +1,44 @@
 import Vapor
 import VaporPostgreSQL
+import Fluent
 
 class BlogPage {
     var title: String
     var content: String
+    var id: Node?
     
     init(title: String, content: String) {
         self.title = title
         self.content = content
+    }
+    
+    required init(node: Node, in context: Context) throws {
+        self.id = try node.extract("id")
+        self.title = try node.extract("title")
+        self.content = try node.extract("content")
+    }
+}
+
+extension BlogPage: Model {
+    func makeNode(context: Context) throws -> Node {
+        return try Node(node: [
+                "id": id,
+                "title": title,
+                "content": content
+            ])
+    }
+}
+
+extension BlogPage: Preparation {
+    static func prepare(_ database: Database) throws {
+        try database.create("blog_pages", closure: { (page) in
+            page.id()
+            page.string("title")
+            page.string("content", length: 10000)
+        })
+    }
+    
+    static func revert(_ database: Database) throws {
+        try database.delete("blog_posts")
     }
 }
