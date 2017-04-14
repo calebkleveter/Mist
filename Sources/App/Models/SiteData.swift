8e5e862bac464d1aba09e7c50bcba14f643584d1
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-14 16:47:59 -0500

    Added theme String property to SiteData model

--- Sources/App/Models/SiteData.swift
+++ Sources/App/Models/SiteData.swift
@@ -4,16 +4,23 @@ import Fluent
 
 class SiteData {
     var id: Node?
+    var theme: String
+    
+    init(theme: String) {
+        self.theme = theme
+    }
     
     required init(node: Node, in context: Context) throws {
         id = try node.extract("id")
+        theme = try node.extract("theme")
     }
 }
 
 extension SiteData: Model {
     func makeNode(context: Context) throws -> Node {
         return try Node(node: [
-                "id": id
+                "id": id,
+                "theme": theme
             ])
     }
 }
@@ -22,6 +29,7 @@ extension SiteData: Preparation {
     static func prepare(_ database: Database) throws {
         try database.create("sitedatas", closure: { (site) in
             site.id()
+            site.string("theme")
         })
     }
     
