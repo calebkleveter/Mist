f092882dd35f6ac3731b27afffb7009b2a9265f3
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-14 16:30:00 -0500

    Added BlogPageController routes to the droplet

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -13,9 +13,11 @@ drop.preparations.append(BlogPage.self)
 drop.preparations.append(Post.self)
 
 let postController = BlogPostController()
+let pageController = BlogPageController()
 
 drop.get { req in
     try postController.addRoutes(to: drop)
+    try pageController.addRoutes(to: drop)
     
     return try drop.view.make("Themes/Default/index",  [
             "pages": try BlogPage.all().makeNode(),
