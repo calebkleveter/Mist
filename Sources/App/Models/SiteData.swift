c6921e538a5b2acf43e05f0bb2abecdbd1c2b1fc
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-14 16:46:05 -0500

    Conformed SiteData to Model protocol

--- Sources/App/Models/SiteData.swift
+++ Sources/App/Models/SiteData.swift
@@ -1,5 +1,31 @@
 import Vapor
+import VaporPostgreSQL
+import Fluent
 
 class SiteData {
+    var id: Node?
     
+    required init(node: Node, in context: Context) throws {
+        id = try node.extract("id")
+    }
+}
+
+extension SiteData: Model {
+    func makeNode(context: Context) throws -> Node {
+        return try Node(node: [
+                "id": id
+            ])
+    }
+}
+
+extension SiteData: Preparation {
+    static func prepare(_ database: Database) throws {
+        try database.create("sitedatas", closure: { (site) in
+            site.id()
+        })
+    }
+    
+    static func revert(_ database: Database) throws {
+        try database.delete("sitedatas")
+    }
 }
