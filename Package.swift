b523adb8d41e28d374c1b7a17bfc8e51e81c4ad5
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-18 11:20:13 -0500

    Package.swift - Exchanged SwiftMark for LeafMarkdown

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 1, minor: 1),
         .Package(url: "../../packages/postgres-admin-panel/.git", Version(0,10,1)),
-        .Package(url: "https://github.com/calebkleveter/SwiftMark.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/brokenhandsio/LeafMarkdown.git", majorVersion: 2, minor: 1)
     ],
     exclude: [
         "Config",
