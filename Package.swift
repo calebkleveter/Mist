ba30e55a783225635b7da55c9e5f6b1e9dda3992
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-10 13:42:33 -0500

    Package.swift

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 let package = Package(
     name: "mist",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/calebkleveter/postgres-admin-panel.git", Version(0,10,1)),
-        .Package(url: "https://github.com/calebkleveter/SwiftMark.git", majorVersion: 1, minor: 2)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor-community/postgresql-provider.git", majorVersion: 2),
+        .Package(url: "https://github.com/calebkleveter/postgres-admin-panel.git", Version(0,10,4)),
+        .Package(url: "https://github.com/calebkleveter/SwiftMark.git", majorVersion: 1, minor: 3)
     ],
     exclude: [
         "Config",
